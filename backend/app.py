17c473768139087a178a91236a357976d4c7272b
karapuzstarka <steame1999@mail.ru>
2025-05-13 16:09:09 +0500

    fix: изменен на mysqlclinet в сервере

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Конфигурация для SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}@"
+    f"mysql+mysqldb://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}@"
     f"{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/{os.getenv('MYSQLDATABASE')}"
 )
 app.config['UPLOAD_FOLDER'] = 'uploads'  # Папка для загрузки файлов
